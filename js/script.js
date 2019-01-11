727ff0fa1fe708c6ddf762975c8977519232585b
Florence <mbababziflorence81@gmail.com>
2019-01-11 11:11:28 +0200

    adding  function

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Business End Logic
+function Player(playerName) {
+  this.totalScore = 0;
+  this.turnScore = 0;
+  this.dice = document.getElementById("die-box");
+  this.playerName = playerName;
+}
